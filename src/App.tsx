0a31a5d5c3b5cf60795684f38860eb62d66b7cf5
raymich <diegoparrales2005@gmail.com>
2025-10-14 21:54:15 -0500

    archivo base para proyecto de sistema de delivery

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import '@styles/index.css'
+
+export default function App() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-3xl font-bold underline text-text-dark">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
