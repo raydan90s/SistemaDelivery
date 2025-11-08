05c2ff975ab344829528e112c583e594c52b5e18
Jorge Bravo <joranbra@espol.edu.ec>
2025-11-08 12:03:17 -0500

    Integración de lógica del registro a AuthContext

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -4,12 +4,22 @@ import { supabase } from '@services/supabase';
 import type { Session, User } from '@supabase/supabase-js';
 
 
+interface RegisterClientArgs {
+  email: string;
+  password: string;
+  nombre: string;
+  apellido: string;
+  celular: string;
+  tipoDocumentoId: string;
+  numeroDocumento: string;
+}
+
 export interface AuthContextType {
   session: Session | null;
   user: User | null;
   isLoading: boolean;
+  registerClient: (args: RegisterClientArgs) => Promise<void>; 
 }
-
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
@@ -19,16 +29,13 @@ export function AuthProvider({ children }: { children: ReactNode }) {
   const [isLoading, setIsLoading] = useState(true); 
 
   useEffect(() => {
-    
     setIsLoading(true);
 
-    
     supabase.auth.getSession().then(({ data: { session } }) => {
       setSession(session);
-      setIsLoading(false); // Terminamos de cargar
+      setIsLoading(false); 
     });
 
-    
     const {
       data: { subscription },
     } = supabase.auth.onAuthStateChange((_event, session) => {
@@ -38,11 +45,50 @@ export function AuthProvider({ children }: { children: ReactNode }) {
     return () => subscription.unsubscribe();
   }, []); 
 
-  
+  // Función para registrar un nuevo cliente
+  const registerClient = async (args: RegisterClientArgs): Promise<void> => {
+    
+    //  Crear el Login en Supabase (auth.users)
+    const { data: authData, error: authError } = await supabase.auth.signUp({
+      email: args.email,
+      password: args.password,
+    });
+
+    if (authError) {
+      // Si hay un error, lo "lanzamos" para que el formulario lo atrape
+      throw new Error(authError.message);
+    }
+
+    // Insertar el Perfil en 'clientes'
+    if (authData.user) {
+      const { error: profileError } = await supabase
+        .from('clientes')
+        .insert({
+          auth_user_id: authData.user.id,
+          nombre: args.nombre,
+          apellido: args.apellido,
+          celular: args.celular,
+          correo: args.email,
+          estado_id: 1, 
+          tipo_cliente_id: 1, 
+          tipo_documento_id: parseInt(args.tipoDocumentoId),
+          numero_documento: args.numeroDocumento,
+        });
+      
+      if (profileError) {
+        throw new Error(profileError.message);
+      }
+    } else {
+      throw new Error("No se pudo crear el usuario en el sistema de autenticación.");
+    }
+  };
+
+
   const value: AuthContextType = {
     session,
     user: session?.user || null,
     isLoading,
+    registerClient, 
   };
 
   return (
